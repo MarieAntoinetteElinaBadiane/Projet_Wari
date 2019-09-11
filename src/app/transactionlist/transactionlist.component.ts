dc6e343cc07674e337ed6703a72354047416ed15
elina <badianeelina8@gmail.com>
2019-09-11 11:03:40 +0200

    first

--- src/app/transactionlist/transactionlist.component.ts
+++ src/app/transactionlist/transactionlist.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
 import { AjoutService } from '../services/ajout.service';
 import { Router } from '@angular/router';
 
